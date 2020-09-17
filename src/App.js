066b0044bafe296c5952018285b6ae16d82262ac
shimna <shimnaa.arangil@gmail.com>
2020-09-17 14:58:47 +0530

    two way binding

--- src/App.js
+++ src/App.js
@@ -11,28 +11,39 @@ class App extends Component {
               {name: 'Rose', age: 12}
           ]
   }
-  changeNameHandler = () => {
+  changeNameHandler = (newname) => {
    //Dont use this this.state.persons[0].name = 'shimna';
    this.setState({
       persons: [
-        {name: 'raja', age: 28 },
+        {name: newname, age: 28 },
         {name: 'rani', age: 11}
     ]}
    );
   }
+
+  changingNameHandler = (event) => {
+    //Dont use this this.state.persons[0].name = 'shimna';
+    this.setState({
+       persons: [
+         {name: "raja", age: 28 },
+         {name: event.target.value, age: 11}
+     ]}
+    );
+   }
 
   render() {
     return (
       <div className="App">
         <h1> My react project, </h1>
         <p> One more sentence ... </p>
-        <button onClick={this.changeNameHandler}> ChangeName</button>
+        <button onClick={() => this.changeNameHandler('hello')}> ChangeName</button>
         <Person1 
         name={this.state.persons[0].name} age={this.state.persons[0].age}>
         </Person1>
         <Person1 
         name={this.state.persons[1].name} age={this.state.persons[1].age}
-        click1={this.changeNameHandler}
+        click1={this.changeNameHandler.bind(this,'Shimna')}
+        change={this.changingNameHandler} 
         > 
         Beautiful!!! 
         </Person1>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -4,6 +4,7 @@ return(
     <div>
         <p onClick={props.click1}>I am a {props.name} and {props.age} years old </p>
         <p>{props.children} </p>
+        <input type="text" onChange={props.change} value={props.name} />
     </div>
 );
 }
