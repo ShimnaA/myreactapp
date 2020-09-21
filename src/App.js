b3872e6942f71ec2285533f92aed139ad8c9a245
shimna <shimnaa.arangil@gmail.com>
2020-09-21 14:01:59 +0530

    blog view

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Validateinput from './Person/Validation';
 import CharComp from './Person/Charcomponent';
+import Blog from './Person/Blog'
 
 
 class App extends Component {
@@ -25,6 +26,11 @@ class App extends Component {
   }
 
   render() {
+
+    const posts = [
+      {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
+      {id: 2, title: 'Installation', content: 'You can install React from npm.'}
+    ];
      const chardatass = this.state.usrInput.split('').map((aa, index) => {
         return <CharComp 
             chardata1={aa} 
@@ -36,14 +42,7 @@ class App extends Component {
     return(
 
       <div className='App'>
-        <ol>
-          <li>Create an input field (in App component) with a change listener which outputs the length of the entered text below it (e.g. in a paragraph).</li>
-          <li>Create a new component (=> ValidationComponent) which receives the text length as a prop</li>
-          <li>Inside the ValidationComponent, either output "Text too short" or "Text long enough" depending on the text length (e.g. take 5 as a minimum length)</li>
-          <li>Create another component (=> CharComponent) and style it as an inline box (=> display: inline-block, padding: 16px, text-align: center, margin: 16px, border: 1px solid black).</li>
-          <li>Render a list of CharComponents where each CharComponent receives a different letter of the entered text (in the initial input field) as a prop.</li>
-          <li>When you click a CharComponent, it should be removed from the entered text.</li>
-        </ol>
+        
       <input type="text"
        onChange={this.txtchangedHandler} 
        value={this.state.usrInput} />
@@ -51,6 +50,7 @@ class App extends Component {
        <Validateinput lendata={this.state.usrInput.length} />
       
        {chardatass}
+       <Blog posts={posts} />
        </div>
 
     );
