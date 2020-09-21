9d32630817e20016e61f674821ae1f946f10b63b
shimna <shimnaa.arangil@gmail.com>
2020-09-21 17:28:12 +0530

    styled-components styling

--- src/App.js
+++ src/App.js
@@ -1,60 +1,134 @@
-import { render } from '@testing-library/react';
+
+//import Validateinput from './Person/Validation';
+//import CharComp from './Person/Charcomponent';
+//import Blog from './Person/Blog'
+
 import React, { Component } from 'react';
 import './App.css';
-import Validateinput from './Person/Validation';
-import CharComp from './Person/Charcomponent';
-import Blog from './Person/Blog'
+import styled from 'styled-components';
+import Person from './Person/Person';
 
+const StyledButton = styled.button`
+  background-color: ${props => props.all ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+  &:hover: {
+    background-color: lightgreen;
+    color: black;
+  }
+
+  `;
 
 class App extends Component {
-  state={
-    usrInput: ''
-  }
- 
-  txtchangedHandler = (event) => {
-    this.setState({
-      usrInput: event.target.value
-    })
-  }
-
-  deletecharHandler = (index) => {
-    const textdata = this.state.usrInput.split('');
-    textdata.splice(index, 1);
-    const updatedtext = textdata.join('');
-    this.setState({usrInput: updatedtext });
-
+  state = {
+    persons: [
+      {id:'p1', name: 'Max', age: 28 },
+      {id:'p2', name: 'Manu', age: 29 },
+      {id:'p3', name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value',
+    showPersons: false
   }
 
-  render() {
+  switchNameHandler = ( newName ) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    } )
+  }
 
-    const posts = [
-      {id: 1, title: 'Hello World', content: 'Welcome to learning React!'},
-      {id: 2, title: 'Installation', content: 'You can install React from npm.'}
-    ];
-     const chardatass = this.state.usrInput.split('').map((aa, index) => {
-        return <CharComp 
-            chardata1={aa} 
-            key={index}
-            delhandle={() => this.deletecharHandler(index)}
-            />;
+  nameChangedHandler = ( event ) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState( { showPersons: !doesShow } );
+  }
+
+  personChangeHandler = (event, perid) => {
+    const personIndex = this.state.persons.findIndex(per => {
+      return per.id === perid;
     });
+    const person = {...this.state.persons[personIndex]};
+    person.name = event.target.value;
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    this.setState({persons: persons});
+
+  }
+  deletePersonHandler = (index) => {
+      const pers = [...this.state.persons];
+      pers.splice(index,1);
+      this.setState({persons: pers});
+  }
+
+  render () {
+    const style = {
+      backgroundColor: 'green',
+      color: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
     
-    return(
 
-      <div className='App'>
-        
-      <input type="text"
-       onChange={this.txtchangedHandler} 
-       value={this.state.usrInput} />
-       <div>The input is {this.state.usrInput}</div>
-       <Validateinput lendata={this.state.usrInput.length} />
-      
-       {chardatass}
-       <Blog posts={posts} />
-       </div>
+    let persons = null;
 
+    if ( this.state.showPersons ) {
+      persons = (
+        <div>
+          {this.state.persons.map((per, index) => {
+             return <Person key={per.id}
+              name={per.name}
+              age={per.age}
+              change={(event) => this.personChangeHandler(event, per.id)}
+              click1={() => this.deletePersonHandler(index)}/>
+          })}  
+        </div>
+      );
+      style.backgroundColor = 'red';
+    }
+
+    //let classes = ['red', 'bold'].join(' ');  //returns a string separted by space "red bold"
+    const classes = [];
+    if (this.state.persons.length <=2 ){
+      classes.push('red')  //classes=['red]
+    }
+    if (this.state.persons.length <=1 ){
+      classes.push('bold')  //classes=[red,'bold]
+    }
+    
+    
+
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <p className={classes.join(' ')}>This is really working!</p>
+        <StyledButton all={this.state.showPersons }
+          onClick={this.togglePersonsHandler}>
+          Toggle Persons </StyledButton>
+        {persons}
+      </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
 export default App;
+
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+
+const Styleddiv = styled.div`
+    width: 60%;
+    margin: 16px;
+    border: 1px solid #eee;
+    box-shadow: 0 2px 3px #ccc;
+    padding: 16px;
+    text-align: center;`;
+
+
+
 const person = (props) => {
 return(
-    <div className="Person">
+   // <div className="Person">
+   <Styleddiv>
         <p onClick={props.click1}>I am a {props.name} and {props.age} years old </p>
         <p>{props.children} </p>
         <input type="text" onChange={props.change} value={props.name} />
 
-    </div>
+    </Styleddiv>
 );
 }
 export default person;
